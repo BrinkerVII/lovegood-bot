259fbd8c7d344a475f57b746621b894646d78c30
BrinkerVII <brinkervii@gmail.com>
2018-04-16 01:38:44 +0200

    Add some DB columns 😉

--- src/main/java/net/brinkervii/lovegood/model/entity/DiscordUser.java
+++ src/main/java/net/brinkervii/lovegood/model/entity/DiscordUser.java
@@ -12,4 +12,11 @@ public class DiscordUser {
 	@Id
 	@Column(name = "id")
 	private Long id;
+
+	@Column(name = "knuts") // Bronze knuts
+	private int knuts = 0;
+	@Column(name = "sickles") // Silver sickles
+	private int sickles = 0; // 29 knuts = 1 sickle
+	@Column(name = "galleons") // Golden galleons
+	private int galleons = 0; // 17 sickles = 1 galleon
 }
